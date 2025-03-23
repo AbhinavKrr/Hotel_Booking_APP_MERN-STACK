2a53ca24b55b1e146419c177f1d596575ad28725
AbhinavKrr <abhinavkumarblog@gmail.com>
2025-03-23 15:51:48 +0530

    Updates

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useSearchContext } from "../contexts/SearchContext"
 import * as apiClient from "../api-client"
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultCard";
+import Pagination from "../components/Pagination";
 
 
 const Search = () => {
@@ -44,9 +45,11 @@ const Search = () => {
                     {/* Todo Sort Options */}
                 </div>
                 {hotelData?.data.map((hotel)=>(
-
                     <SearchResultsCard hotel={hotel} key={hotel._id}/>
                 ))}
+                <div>
+                    <Pagination page={hotelData?.pagination.page || 1} pages={hotelData?.pagination.pages || 1} onPageChange={(page)=>setPage(page)}/>
+                </div>
             </div>
         </div>
     );
